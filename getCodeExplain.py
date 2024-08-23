f9495f74d84f113d41edd3a83a2c23e24e2802ec
claitonneri <claitonneri@hotmail.com>
2024-08-23 00:16:40 -0300

    Adiciona chamada de API para obter explicação de código

--- getCodeExplain.py
+++ getCodeExplain.py
@@ -11,4 +11,15 @@ client = OpenAI(
   api_key=os.getenv('OPENAI_API_KEY')
 )
 
-print(client.models.list())
+stream = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+      {"role": "system", "content": "Você irá receber um código e gostaria de uma explicação detalhada sobre o funcionamento do código."},
+      {"role": "user", "content": "freecodecamp = 'FREECODECAMP' print(f'{freecodecamp.lower()}')"}
+    ],
+    stream=True,
+)
+
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
